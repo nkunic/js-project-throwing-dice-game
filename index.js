cc2f0287497756e3b68880ce4437fc688741fe71
nkunic <natasa.kunic@gmail.com>
2022-05-24 16:21:35 +0200

    3 files modified

--- index.js
+++ index.js
@@ -3,71 +3,98 @@ import './style.css';
 
 // Write Javascript code!
 
+var player1Div = document.getElementsByClassName('player1')[0];
+var player2Div = document.getElementsByClassName('player2')[0];
+var startBtn = document.getElementById('startBtn');
+var container = document.getElementsByClassName('container')[0];
+var circle = document.getElementsByClassName('circle')[0];
+var player1 = '';
+var player2 = '';
+var player1TotalScore = 0;
+var player2TotalScore = 0;
+var player1Score = document.getElementById('player1Score');
+var player2Score = document.getElementById('player2Score');
+var player1Current = document.getElementById('player1Current');
+var player2Current = document.getElementById('player2Current');
+var player1Name = document.getElementById('player1Name');
+var player2Name = document.getElementById('player2Name');
+var player1Btn = document.getElementById('player1Btn');
+var player2Btn = document.getElementById('player2Btn');
+var startAgain = document.getElementById('startAgain');
+var rollingCounter = 1;
 
-/* Players*/
-// var player1 = 'Danilo';
-// var player2 = 'Ivan';
-var player1 = prompt('Player 1');
-var player2 = prompt('Player 2');
+startBtn.addEventListener('click', askForNames);
+startAgain.addEventListener('click', clearInputs);
 
-/* Round */
-var round = 0;
+function clearInputs() {
+  rollingCounter = 1;
+  // player1Name.innerHTML = "Player1";
+  // player2Name.innerHTML = "Player2";
+  player1Current.innerHTML = '0';
+  player2Current.innerHTML = '0';
+  player1Score.innerHTML = '0';
+  player2Score.innerHTML = '0';
+  player1Btn.removeAttribute('disabled');
+  player2Btn.removeAttribute('disabled');
+  player1Div.style.background = 'none';
+  player2Div.style.background = 'none';
+  startAgain.style.display = 'none';
+  player2TotalScore = 0;
+  player1TotalScore = 0;
+  circle.innerHTML = '1';
+}
+function askForNames() {
+  player1 = prompt('Unesi ime prvog igrača');
+  player2 = prompt('Unesi ime drugog igrača');
+  displayGame();
+}
+function displayGame() {
+  startBtn.style.display = 'none';
+  container.style.display = 'block';
+  player1Name.innerHTML = player1;
+  player2Name.innerHTML = player2;
+  player1Btn.addEventListener('click', player1Roll);
+  player2Btn.addEventListener('click', player2Roll);
+}
+function player1Roll() {
+  rollingCounter += 0.5;
 
-/* Player score */
-var player1Score = 0;
-var player2Score = 0;
-
-function bacanjeKockice() {
-  /* Eliminacija skakanja konzole */
-  console.clear();
-
-  /* Round count */
-  round++;
-
-  /* Dice */
-  // var k1 = Math.floor(Math.random() * 6);
-  // var k2 = Math.floor(Math.random() * 6);
-  var kockica1 = Math.ceil(Math.random() * 6);
-  var kockica2 = Math.ceil(Math.random() * 6);
-
-  /* Player score */
-  player1Score += kockica1;
-  player2Score += kockica2;
-
-  /* Dice trowed */
-  //console.log(kockica1, kockica2);
-  console.log('*** ROUND ' + round + ' ***');
-  console.log(player1 + ':' + kockica1 + ' --- ' + kockica2 + ':' + player2);
-
-  /* Current score */
-  console.log(
-    player1 +
-      '(' +
-      player1Score +
-      ')' +
-      ' : ' +
-      '(' +
-      player2Score +
-      ')' +
-      player2
-  );
-
-  /* Game ends after 10 rounds */
-  if (round === 10) {
-    winner();
+  this.setAttribute('disabled', 'disabled');
+  player2Btn.removeAttribute('disabled');
+  var rand = Math.floor(Math.random() * 6 + 1);
+  player1TotalScore += rand;
+  player1Current.innerHTML = rand;
+  player1Score.innerHTML = player1TotalScore;
+  if (Math.floor(rollingCounter) < 11) {
+    circle.innerHTML = Math.floor(rollingCounter);
   } else {
-    setTimeout(bacanjeKockice, 3000);
+    gameOver();
   }
 }
-
-bacanjeKockice();
-
-function winner() {
-  if (player1Score > player2Score) {
-    console.log('Winner: ' + player1 + '!');
-  } else if (player1Score < player2Score) {
-    console.log('Winner: ' + player2 + '!');
+function player2Roll() {
+  rollingCounter += 0.5;
+  this.setAttribute('disabled', 'disabled');
+  player1Btn.removeAttribute('disabled');
+  var rand = Math.floor(Math.random() * 6 + 1);
+  player2TotalScore += rand;
+  player2Current.innerHTML = rand;
+  player2Score.innerHTML = player2TotalScore;
+  if (Math.floor(rollingCounter) < 11) {
+    circle.innerHTML = Math.floor(rollingCounter);
   } else {
-    console.log('Draw: no winner!');
+    gameOver();
+  }
+}
+function gameOver() {
+  player1Btn.setAttribute('disabled', 'disabled');
+  player2Btn.setAttribute('disabled', 'disabled');
+  startAgain.style.display = 'block';
+  if (player1TotalScore > player2TotalScore) {
+    player1Div.style.background = '#5cb85c';
+  } else if (player1TotalScore < player2TotalScore) {
+    player2Div.style.background = '#5cb85c';
+  } else {
+    player1Div.style.background = '#c9302c';
+    player2Div.style.background = '#c9302c';
   }
 }
